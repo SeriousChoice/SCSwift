38aad08a445d33c6e397a1b3eb09074b4b7cb33d
Seriouschoice <seriouschoice9@gmail.com>
2022-01-15 10:53:28 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,13 +14,16 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/PureLayout/PureLayout.git", from: "3.1.9"),
+        .package(name: "PureLayout", url: "https://github.com/PureLayout/PureLayout.git", from: "3.1.9")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SCSwift",
+            dependencies: [
+                "PureLayout",
+            ],
             path: "SCSwift"
         )
     ]
